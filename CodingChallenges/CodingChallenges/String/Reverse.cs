ed2a7a1cc17555adb17b4217864a3aed8fb8f849
aurobinda jena <aurobindajena90@gmail.com>
2025-03-11 10:43:10 +0530

    reverse string

--- CodingChallenges/CodingChallenges/String/Reverse.cs
+++ CodingChallenges/CodingChallenges/String/Reverse.cs
@@ -41,4 +41,58 @@ public class Reverse
         Console.WriteLine($"{toReverse} -After Reverse- {results}");
         return results;
     }
+
+    public string ReverseStringRecursively(string toReverse)
+    {
+        if (toReverse.Length == 1)
+        {
+            return toReverse;
+        }
+        else
+        {
+            return ReverseStringRecursively(toReverse.Substring(1)) + toReverse[0] ;
+        }
+    }
+
+    public void ReverseFileContent(string path)
+    {
+        string fileContent = File.ReadAllText(path);
+        File.WriteAllText(path, new string (fileContent.Reverse().ToArray()));
+    }
+
+    public string ReverseEveryKCharacters(string toReverse, int k)
+    {
+        char[] arr = toReverse.ToCharArray();
+
+        for (int i = 0; i < arr.Length; i += k)
+        {
+            int left = i;
+            int right = Math.Min(i + k - 1, arr.Length - 1); 
+            while (left < right)
+            {
+                char temp = arr[left];
+                arr[left] = arr[right];
+                arr[right] = temp;
+                left++;
+                right--;
+            }
+        }
+        return new string(arr);
+    }
+
+    public void ReverseCharArray(char[] arr)
+    {
+        int left = 0, right = arr.Length - 1;
+        while (left < right)
+        {
+            (arr[left], arr[right]) = (arr[right], arr[left]);
+            left++;
+            right--;
+        }
+        //for (int left = 0, right = arr.Length - 1; left < right; left++, right--)
+        //{
+        //    (arr[left], arr[right]) = (arr[right], arr[left]);
+        //}
+    }
+
 }
